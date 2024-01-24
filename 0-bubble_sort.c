abf1c95dd52f48d313badea05481ab64c67e742a
Ken-wangari <wangariken50@gmail.com>
2024-01-24 12:53:55 -0800

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -25,21 +25,21 @@ void swp_integers(int *a, int *b)
 void bubble_sort(int *array, size_t size)
 {
     size_t i, len = size;
-    bool bubbly = false;
+    bool bub = false;
 
     if (array == NULL || size < 2)
         return;
 
-    while (bubbly == false)
+    while (bub == false)
     {
-        bubbly = true;
+        bub = true;
         for (i = 0; i < len - 1; i++)
         {
             if (array[i] > array[i + 1])
             {
                 swp_integers(array + i, array + i + 1);
                 print_array(array, size);
-                bubbly = false;
+                bub = false;
             }
         }
         len--;
